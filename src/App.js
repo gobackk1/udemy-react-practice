2e90a7566f0467bbd36e8059b1c536869ef9b109
川端　健悟 <k.kwbt@icloud.com>
2020-04-16 09:39:39 +0900

    create class component and funtional

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import { render } from "@testing-library/react";
 
-function App() {
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bbb">class</label>
+//         <input type="text" onChange={() => console.log("clicked")} />
+//       </React.Fragment>
+//     );
+//   }
+// }
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bbb">functional</label>
+//       <input type="text" onChange={() => console.log("clicked")} />
+//     </React.Fragment>
+//   );
+// }
+
+const App = () => {
   return (
-    <React.Fragment>
-      <label htmlFor="bbb">bbb</label>
-      <input type="text" onChange={() => console.log("clicked")} />
-    </React.Fragment>
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
   );
-}
+};
+
+const Cat = () => {
+  return <div>Miaw</div>;
+};
 
 export default App;
